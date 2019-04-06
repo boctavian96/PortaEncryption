8781ccd24a1727bb60e3b26c9cf9e5f928fb0c3b
boctavian96 <boctavian96@yahoo.com>
2019-04-07 02:14:23 +0300

    Small corrections.

--- includes/PortaTable.h
+++ includes/PortaTable.h
@@ -6,8 +6,12 @@ class PortaTable
 	public: 
 		PortaTable();
 		virtual ~PortaTable();
+        bool isKey(char key);
 	protected:
 	private:
+        char m_pk1;
+        char m_pk2;
+        map<const char key, char element> m_elements;
 };
 
 #endif // PORTATABLE_H
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "includes/Porta.h"
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+    Porta *p;
+    p = new Porta();
+    cout << "I've created Porta !!!" << "\n";
+    cout << "Hello" << "\n";
+
+    return 0;
+}
